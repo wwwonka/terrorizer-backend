cc14e2f78a869fccff5d0f6ac177012824c2c61f
wwwonka <je.mange.du.boeuf@gmail.com>
2023-11-23 02:39:36 -0500

    added redis

--- medusa-config.js
+++ medusa-config.js
@@ -65,12 +65,12 @@ const plugins = [
 ];
 
 const modules = {
-	// eventBus: {
-	// 	resolve: "@medusajs/event-bus-redis",
-	// 	options: {
-	// 		redisUrl: REDIS_URL,
-	// 	},
-	// },
+	eventBus: {
+		resolve: "@medusajs/event-bus-redis",
+		options: {
+			redisUrl: REDIS_URL,
+		},
+	},
 	// cacheService: {
 	// 	resolve: "@medusajs/cache-redis",
 	// 	options: {
@@ -87,7 +87,7 @@ const projectConfig = {
 	database_url: DATABASE_URL,
 	admin_cors: ADMIN_CORS,
 	// Uncomment the following lines to enable REDIS
-	// redis_url: REDIS_URL,
+	redis_url: REDIS_URL,
 };
 
 
