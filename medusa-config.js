903f12cac6ee16eba237fa67afc33705d0c52976
wwwonka <je.mange.du.boeuf@gmail.com>
2023-11-23 02:28:19 -0500

    removed redis

--- medusa-config.js
+++ medusa-config.js
@@ -65,18 +65,18 @@ const plugins = [
 ];
 
 const modules = {
-	eventBus: {
-		resolve: "@medusajs/event-bus-redis",
-		options: {
-			redisUrl: REDIS_URL,
-		},
-	},
-	cacheService: {
-		resolve: "@medusajs/cache-redis",
-		options: {
-			redisUrl: REDIS_URL,
-		},
-	},
+	// eventBus: {
+	// 	resolve: "@medusajs/event-bus-redis",
+	// 	options: {
+	// 		redisUrl: REDIS_URL,
+	// 	},
+	// },
+	// cacheService: {
+	// 	resolve: "@medusajs/cache-redis",
+	// 	options: {
+	// 		redisUrl: REDIS_URL,
+	// 	},
+	// },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
@@ -87,7 +87,7 @@ const projectConfig = {
 	database_url: DATABASE_URL,
 	admin_cors: ADMIN_CORS,
 	// Uncomment the following lines to enable REDIS
-	redis_url: REDIS_URL,
+	// redis_url: REDIS_URL,
 };
 
 
